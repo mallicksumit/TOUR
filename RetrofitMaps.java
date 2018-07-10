5947859e0a4d18634a612fd89248440f55b4895a
Sumit Mallick <mallick.23.sumit@gmail.com>
2018-07-10 23:51:54 +0530

    Update RetrofitMaps.java

--- RetrofitMaps.java
+++ RetrofitMaps.java
@@ -10,7 +10,7 @@ public interface RetrofitMaps {
      * Retrofit get annotation with our URL
      * And our method that will return us details of student.
      */
-    @GET("api/place/nearbysearch/json?sensor=true&key=AIzaSyAKFbBnnGqe0rv6RCnIVFAJZL4uBaoGndc")
+    @GET("api/place/nearbysearch/json?sensor=true&key=xxxxxxxxx")
     Call<MainPLaces> getNearbyPlaces(@Query("type") String type, @Query("location") String location, @Query("radius") int radius);
 
 }
